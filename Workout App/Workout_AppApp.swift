831db8baae18840d45c059f819d38ed9ae298769
Jonathan Young <jonathanyoung7718@gmail.com>
2025-06-26 07:48:59 -0500

    Initial Commit

---
+++ Workout App/Workout_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Workout_AppApp.swift
+//  Workout App
+//
+//  Created by Jonathan Young on 6/26/25.
+//
+
+import SwiftUI
+
+@main
+struct Workout_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
