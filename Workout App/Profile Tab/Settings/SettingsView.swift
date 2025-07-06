73c84f30d761c93b84700c9e179c7c266590dcbe
Jonathan Young <jonathanyoung7718@gmail.com>
2025-07-06 16:40:32 -0500

    Removed font size

--- Workout App/Profile Tab/Settings/SettingsView.swift
+++ Workout App/Profile Tab/Settings/SettingsView.swift
@@ -8,40 +8,10 @@
 import SwiftUI
 
 struct SettingsView: View {
-    @State private var fontSize: CGFloat
-    
-    init() {
-        self.fontSize = {
-            if UserDefaults.standard.object(forKey: "fontSize") == nil {
-                return 16
-            }
-            return CGFloat(UserDefaults.standard.integer(forKey: "fontSize"))
-        }()
-    }
     
     var body: some View {
         Form {
-            Section("Font Size") {
-                VStack(alignment: .leading) {
-                    Slider(value: $fontSize, in: 12...18, step: 1) {
-                        Text("Font Size: \(Int(fontSize))")
-                    }
-                    .padding(.horizontal, 10)
-                    .onChange(of: fontSize) { _, newValue in
-                        UserDefaults.standard.set(Int(newValue), forKey: "fontSize")
-                    }
-                    
-                    // Simulated tick marks
-                    HStack {
-                        ForEach(12...18, id: \.self) { value in
-                            Text("\(value)")
-                                .font(.caption2)
-                                .frame(maxWidth: .infinity)
-                                .foregroundStyle(.secondary)
-                        }
-                    }
-                }
-            }
+            
         }
     }
 }
